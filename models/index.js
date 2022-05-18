8b3b12824f6ff38e067168cd404339b3e27ef366
HanInJu <haninju95@gmail.com>
2022-05-18 13:34:41 +0900

    ADD: Like DB 매핑

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const config = require('../config/config')[env];
 const User = require('./user');
 const Post = require('./post');
 const Hashtag = require('./hashtag');
+const Like = require('./like')
 
 const db = {};
 const sequelize = new Sequelize(
@@ -14,13 +15,16 @@ db.sequelize = sequelize;
 db.User = User;
 db.Post = Post;
 db.Hashtag = Hashtag;
+db.Like = Like;
 
 User.init(sequelize);
 Post.init(sequelize);
 Hashtag.init(sequelize);
+Like.init(sequelize);
 
 User.associate(db);
 Post.associate(db);
 Hashtag.associate(db);
+Like.associate(db);
 
 module.exports = db;
