e1db9d599cd0138f711c714bbfa9c86ca9fdcd66
shfkjsflslfjsfls <57622965+shfkjsflslfjsfls@users.noreply.github.com>
2022-05-24 01:43:45 +0900

    ADD profile

--- routes/page.js
+++ routes/page.js
@@ -12,9 +12,41 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/profile', isLoggedIn, (req, res) => {
-  res.render('profile', { title: 'Profile - NOD' });
+router.get('/profile', isLoggedIn, async(req, res) => {
+  try {          
+  const page = Number(req.query.page || 1); // 값이 없다면 기본값으로 1 사용
+  const perPage = Number(req.query.perPage || 10);
+   /* const total = await Post.countDocument({});  아래 total지우고 이부분을 총 document수 받아오는걸로 변경 */
+  const total = 20;
+  const totalPage = Math.ceil(total / perPage);
+  const posts = await Post.findAll({
+    include: {
+      model: User,
+      attributes: ['id', 'nickname','email'],
+    },
+    order: [['createdAt', 'DESC']],
+   /*  sort: {'createdAt':-1}, */
+    offset: perPage * (page - 1),
+    limit:perPage,
+    })
+    
+
+    res.render('profile', {
+    title: 'NOD',
+    twits: posts,
+    totalPage: totalPage,
+    });
+    
+} catch (err) {
+  console.error(err);
+  next(err);
+}
+    
+
 });
+
+
+
 
 router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', { title: 'Join to - NOD' });
